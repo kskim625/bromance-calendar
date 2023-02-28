af3182b3dd51d70bee6244d52a985949f8daf4cd
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-03-01 00:54:09 +0900

    etc. 4월 첫째 주 일정 수정

--- src/data/data.js
+++ src/data/data.js
@@ -7,7 +7,7 @@ const data = [
   { year: '2023', month: '3', date: '12', iso: '2023-03-12', location: '용산 6구장', time: '오전 6-8시', status: '투표 예정', _id: 'month3data2' },
   { year: '2023', month: '3', date: '19', iso: '2023-03-19', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month3data3' },
   { year: '2023', month: '3', date: '25', iso: '2023-03-25', location: '용산 3구장', time: '오전 7-9시', status: '투표 예정', _id: 'month3data4' },
-  { year: '2023', month: '4', date: '1', iso: '2023-04-01', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month4data1' },
+  { year: '2023', month: '4', date: '2', iso: '2023-04-02', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month4data1' },
   { year: '2023', month: '4', date: '8', iso: '2023-04-08', location: '용산 7구장', time: '오전 7-9시', status: '투표 예정', _id: 'month4data2' },
   { year: '2023', month: '4', date: '16', iso: '2023-04-16', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month4data3' },
   { year: '2023', month: '4', date: '22', iso: '2023-04-22', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month4data4' },
