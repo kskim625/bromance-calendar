cd5817759b643fd942ddab024eacc694d5a3c73f
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-05-01 00:25:19 +0900

    etc. 6월 일정 업데이트

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,4 @@
 const data = [
-  { year: '2023', month: '3', date: '4', iso: '2023-03-04', location: '용산 1구장', time: '오전 7-9시', status: '매치 완료', _id: 'month3data1' },
-  { year: '2023', month: '3', date: '12', iso: '2023-03-12', location: '용산 6구장', time: '오전 6-8시', status: '구장 양도', _id: 'month3data2' },
-  { year: '2023', month: '3', date: '19', iso: '2023-03-19', location: '용산 4구장', time: '오전 6-8시', status: '매치 완료', _id: 'month3data3' },
-  { year: '2023', month: '3', date: '25', iso: '2023-03-25', location: '용산 3구장', time: '오전 7-9시', status: '구장 양도', _id: 'month3data4' },
   { year: '2023', month: '4', date: '2', iso: '2023-04-02', location: '용산 2구장', time: '오전 6-8시', status: '매치 완료', _id: 'month4data1' },
   { year: '2023', month: '4', date: '8', iso: '2023-04-08', location: '용산 7구장', time: '오전 7-9시', status: '투표 예정', _id: 'month4data2' },
   { year: '2023', month: '4', date: '16', iso: '2023-04-16', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month4data3' },
@@ -12,6 +8,10 @@ const data = [
   { year: '2023', month: '5', date: '14', iso: '2023-05-14', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month5data2' },
   { year: '2023', month: '5', date: '20', iso: '2023-05-20', location: '용산 7구장', time: '오전 7-9시', status: '투표 예정', _id: 'month5data3' },
   { year: '2023', month: '5', date: '28', iso: '2023-05-28', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month5data4' },
+  { year: '2023', month: '6', date: '3', iso: '2023-06-03', location: '용산 6구장', time: '오전 6-8시', status: '투표 예정', _id: 'month6data1' },
+  { year: '2023', month: '6', date: '10', iso: '2023-06-10', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month6data2' },
+  { year: '2023', month: '6', date: '18', iso: '2023-06-18', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month6data3' },
+  { year: '2023', month: '6', date: '25', iso: '2023-06-25', location: '용산 1구장', time: '오전 7-9시', status: '투표 예정', _id: 'monthdata4' },
 ];
 
 export default data;
