eaaa7d9f99f52e876e9c03f1c04f30702d18067c
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-07-19 20:55:01 +0900

    etc. 8월 일정 업데이트

--- src/data/data.js
+++ src/data/data.js
@@ -1,17 +1,15 @@
 const data = [
-  { year: '2023', month: '5', date: '7', iso: '2023-05-07', location: '용산 5구장', time: '오전 7-9시', status: '매치 완료', _id: 'month5data1' },
-  { year: '2023', month: '5', date: '14', iso: '2023-05-14', location: '용산 2구장', time: '오전 6-8시', status: '매치 완료', _id: 'month5data2' },
-  { year: '2023', month: '5', date: '20', iso: '2023-05-20', location: '용산 7구장', time: '오전 7-9시', status: '매치 완료', _id: 'month5data3' },
-  { year: '2023', month: '5', date: '28', iso: '2023-05-28', location: '용산 4구장', time: '오전 6-8시', status: '자체전', _id: 'month5data4' },
   { year: '2023', month: '6', date: '3', iso: '2023-06-03', location: '용산 6구장', time: '오전 6-8시', status: '자체전', _id: 'month6data1' },
-  { year: '2023', month: '6', date: '10', iso: '2023-06-10', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month6data2' },
-  { year: '2023', month: '6', date: '18', iso: '2023-06-18', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month6data3' },
-  { year: '2023', month: '6', date: '25', iso: '2023-06-25', location: '용산 1구장', time: '오전 7-9시', status: '투표 예정', _id: 'monthdata4' },
-  { year: '2023', month: '7', date: '1', iso: '2023-07-01', location: '용산 7구장', time: '오전 7-9시', status: '투표 예정', _id: 'month7data1' },
-  { year: '2023', month: '7', date: '9', iso: '2023-07-09', location: '용산 4구장', time: '오전 6-8시', status: '투표 예정', _id: 'month7data2' },
-  { year: '2023', month: '7', date: '15', iso: '2023-07-15', location: '용산 2구장', time: '오전 6-8시', status: '투표 예정', _id: 'month7data3' },
-  { year: '2023', month: '7', date: '23', iso: '2023-07-23', location: '용산 6구장', time: '오전 6-8시', status: '투표 예정', _id: 'month7data4' },
+  { year: '2023', month: '6', date: '10', iso: '2023-06-10', location: '용산 4구장', time: '오전 6-8시', status: '매치 완료', _id: 'month6data2' },
+  { year: '2023', month: '6', date: '18', iso: '2023-06-18', location: '용산 2구장', time: '오전 6-8시', status: '매치 완료', _id: 'month6data3' },
+  { year: '2023', month: '6', date: '25', iso: '2023-06-25', location: '용산 1구장', time: '오전 7-9시', status: '타팀 양도', _id: 'monthdata4' },
+  { year: '2023', month: '7', date: '1', iso: '2023-07-01', location: '용산 7구장', time: '오전 7-9시', status: '매치 완료', _id: 'month7data1' },
+  { year: '2023', month: '7', date: '9', iso: '2023-07-09', location: '용산 4구장', time: '오전 6-8시', status: '매치 완료', _id: 'month7data2' },
+  { year: '2023', month: '7', date: '15', iso: '2023-07-15', location: '용산 2구장', time: '오전 6-8시', status: '타팀 양도', _id: 'month7data3' },
+  { year: '2023', month: '7', date: '23', iso: '2023-07-23', location: '용산 6구장', time: '오전 6-8시', status: '매치 완료', _id: 'month7data4' },
   { year: '2023', month: '7', date: '30', iso: '2023-07-30', location: '용산 3구장', time: '오전 7-9시', status: '투표 예정', _id: 'month7data5' },
+  { year: '2023', month: '8', date: '20', iso: '2023-08-20', location: '용산 7구장', time: '오전 7-9시', status: '투표 예정', _id: 'month8data1' },
+  { year: '2023', month: '8', date: '27', iso: '2023-08-27', location: '용산 5구장', time: '오전 7-9시', status: '투표 예정', _id: 'month8data2' },
 ];
 
 export default data;
